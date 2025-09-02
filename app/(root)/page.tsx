b4d5b74c1c5d7516df7a04743458c935425ac089
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2025-09-02 14:52:25 -0300

    feat: implement product validation schema and update product types; enhance product listing with latest products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,13 @@ import ProductList from "@/components/shared/product/product-list";
 // import sampleData from "@/db/sample-data";
 import { getLatestProducts } from "@/lib/actions/product.action";
 
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const Homepage = async () => {
+  await delay(2000);
   const latestProducts = await getLatestProducts();
 
+  console.log("getLatestProducts", latestProducts);
   return (
     <>
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />

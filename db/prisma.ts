0b2a6cea875bfc2fd2b349bcbcbd5970a6d13667
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2025-09-02 15:25:44 -0300

    feat: configure Prisma with Neon adapter; update environment variables and implement latest products limit

---
+++ db/prisma.ts
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
+import { PrismaClient } from "@/lib/generated/prisma";
+neonConfig.webSocketConstructor = ws;
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaNeon({ connectionString });
+
+// Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
+export const prisma = new PrismaClient({ adapter }).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
+        },
+      },
+    },
+  },
+});

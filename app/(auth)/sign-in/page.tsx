6d5fc0b1d7ce245b730ab21ac18cfdc17c2e1d80
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2025-09-05 15:26:31 -0300

    feat: adicionar formulário de autenticação com campos de e-mail e senha; integrar ao layout da página de login

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -9,6 +9,7 @@ import { APP_NAME } from "@/lib/constants";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import CredentialsSignInForm from "./credentials-signin-form";
 
 export const metadata: Metadata = {
   title: "Sign In",
@@ -34,7 +35,9 @@ const SignInPage = () => {
           </CardDescription>
         </CardHeader>
 
-        <CardContent className="space-y-4">{/* Form Aqui */}</CardContent>
+        <CardContent className="space-y-4">
+          <CredentialsSignInForm />
+        </CardContent>
       </Card>
     </div>
   );

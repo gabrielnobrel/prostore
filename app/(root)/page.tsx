5efd860455d757116e254eb6247ef48b9fa6cdf1
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2025-09-05 14:55:47 -0300

    feat: corrigir importações de ações de produtos para o arquivo correto

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import ProductList from "@/components/shared/product/product-list";
 // import sampleData from "@/db/sample-data";
-import { getLatestProducts } from "@/lib/actions/product.action";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();

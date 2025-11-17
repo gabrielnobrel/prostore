24b7d3e7d9d979f44514031e6297b3e02ba8f3af
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2025-11-17 15:49:41 -0300

    feat: adicionar funcionalidade para criar e atualizar produtos com validação

---
+++ components/admin/product-form.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { productDefaultValues } from "@/lib/constants";
+import { insertProductSchema, updateProductSchema } from "@/lib/validators";
+import { Product } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { Form } from "../ui/form";
+
+const ProductForm = ({
+  type,
+  product,
+  produdctId,
+}: {
+  type: "Create" | "Update";
+  product?: Product;
+  produdctId?: string;
+}) => {
+  const router = useRouter();
+  const schema = type === "Update" ? updateProductSchema : insertProductSchema;
+
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues:
+      product && type === "Update" ? product : productDefaultValues,
+  });
+
+  return (
+    <Form {...form}>
+      <form className="space-y-8">
+        <div className="flex flex-col md:flex-row gap-5">
+          {/* Name */}
+          {/* Slug */}
+        </div>
+        <div className="flex flex-col md:flex-row gap-5">
+          {/* Category */}
+          {/* Brand */}
+        </div>
+        <div className="flex flex-col md:flex-row gap-5">
+          {/* Price */}
+          {/* Stock */}
+        </div>
+        <div className="upload-field flex flex-col md:flex-row gap-5">
+          {/* Images */}
+        </div>
+        <div className="upload-field">{/* isFeatured */}</div>
+        <div>{/* Description */}</div>
+        <div>{/* Submit */}</div>
+      </form>
+    </Form>
+  );
+};
+
+export default ProductForm;
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -4,6 +4,8 @@ import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
 import { prisma } from "@/db/prisma";
 import { convertToPlainObject, formatError } from "../utils";
 import { revalidatePath } from "next/cache";
+import { insertProductSchema, updateProductSchema } from "../validators";
+import z from "zod";
 
 // Get latest products
 export async function getLatestProducts() {
@@ -65,6 +67,47 @@ export async function deleteProduct(id: string) {
     revalidatePath("/admin/products");
 
     return { success: true, message: "Product deleted successfully" };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// Create a product
+export async function createProduct(data: z.infer<typeof insertProductSchema>) {
+  try {
+    // Validação
+    const product = insertProductSchema.parse(data);
+    await prisma.product.create({
+      data: product,
+    });
+
+    revalidatePath(`/admin/products`);
+
+    return { success: true, message: "Product created successfully" };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// Update a product
+export async function updateProduct(data: z.infer<typeof updateProductSchema>) {
+  try {
+    // Validação
+    const product = updateProductSchema.parse(data);
+    const productExist = await prisma.product.findFirst({
+      where: { id: product.id },
+    });
+
+    if (!productExist) throw new Error("Product not found");
+
+    await prisma.product.update({
+      where: { id: product.id },
+      data: product,
+    });
+
+    revalidatePath(`/admin/products`);
+
+    return { success: true, message: "Product updated successfully" };
   } catch (error) {
     return { success: false, message: formatError(error) };
   }

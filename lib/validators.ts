a4f3191fcd052c6901ea22b30efe915b5a4e5c40
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2026-05-07 17:34:35 -0300

    feat(users): adicionar página e formulário para atualização de usuário

--- lib/validators.ts
+++ lib/validators.ts
@@ -6,7 +6,7 @@ const currency = z
   .string()
   .refine(
     (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
-    "Price must have exactly two decimal places"
+    "Price must have exactly two decimal places",
   );
 
 // Schema for inserting products
@@ -126,4 +126,10 @@ export const paymentResultSchema = z.object({
 export const updateProfileSchema = z.object({
   name: z.string().min(3, "Name must be at least 3 characters"),
   email: z.string().min(3, "Email must be at least 3 characters"),
+});
+
+// Schema to update users
+export const updateUserSchema = updateProfileSchema.extend({
+  id: z.string().min(1, "ID is required"),
+  role: z.string().min(1, "Role is required"),
 });

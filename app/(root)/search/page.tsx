74d2a6767bb4bb451bf038873ef8deabdfad95b4
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2026-05-15 16:01:58 -0300

    feat(search): adicionar função para construir URL de filtros na página de busca

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -20,6 +20,31 @@ const SearchPage = async (props: {
     page = "1",
   } = await props.searchParams;
 
+  // Construct filter url
+  const getFilterUrl = ({
+    c,
+    s,
+    p,
+    r,
+    pg,
+  }: {
+    c?: string;
+    s?: string;
+    p?: string;
+    r?: string;
+    pg?: string;
+  }) => {
+    const params = { q, category, price, rating, sort, page };
+
+    if (c) params.category = c;
+    if (s) params.sort = s;
+    if (p) params.price = p;
+    if (r) params.rating = r;
+    if (pg) params.page = pg;
+
+    return `/search?${new URLSearchParams(params).toString()}`;
+  };
+
   const products = await getAllProducts({
     query: q,
     category,
@@ -31,7 +56,10 @@ const SearchPage = async (props: {
 
   return (
     <div className="grid md:grid-cols-5 md:gap-5">
-      <div className="filter-links">{/* FILTERS */}</div>
+      <div className="filter-links">
+        {/* FILTERS */}
+        URL: {getFilterUrl({ c: "Mens Swate" })}
+      </div>
 
       <div className="md:col-span-4 space-y-4">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">

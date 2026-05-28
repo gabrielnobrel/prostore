d7dc1dfe67b0455417e315cfbecd0c71879ce623
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2026-05-28 13:57:20 -0300

    feat(review): adicionar componente de lista de avaliações e integrar na página de detalhes do produto

---
+++ app/(root)/product/[slug]/review-list.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Review } from "@/types";
+import Link from "next/link";
+import { useState } from "react";
+
+const ReviewList = ({
+  userId,
+  productId,
+  productSlug,
+}: {
+  userId: string;
+  productId: string;
+  productSlug: string;
+}) => {
+  const [reviews, setReviews] = useState<Review[]>([]);
+
+  return (
+    <>
+      <div className="spacey-4">
+        {reviews.length === 0 && <div>No reviews yet</div>}
+
+        {userId ? (
+          <>{/* Review form here */}</>
+        ) : (
+          <div>
+            Please{" "}
+            <Link
+              className="text-blue-700 px-2"
+              href={`/sign-in?callbackUrl=/product${productSlug}`}
+            >
+              Sign-in
+            </Link>
+            to write a review
+          </div>
+        )}
+
+        <div className="flex felx-col gap-3">{/* REVIEWS HERE */}</div>
+      </div>
+    </>
+  );
+};
+
+export default ReviewList;

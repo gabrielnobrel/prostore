47bdbf1921edfff26fbd60b0059142f57dc4cd65
Gabriel Nobrel <gabriel_nobresantos@hotmail.com>
2026-09-09 09:30:28 -0300

    feat(payment): integrar pagamento com Stripe e adicionar componente StripePayment

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation";
 import OrderDetailsTable from "./order-details-table";
 import { ShippingAddress } from "@/types";
 import { auth } from "@/auth";
+import Stripe from "stripe";
 
 export const metadata: Metadata = {
   title: "Order Details",
@@ -17,12 +18,29 @@ const OrderDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
 
   const session = await auth();
 
+  let client_secret = null;
+
+  // Check if is not paid and using stripe
+  if (order.paymentMethod === "stripe" && !order.isPaid) {
+    // Init stripe instance
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+    // Create payment intent
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(Number(order.totalPrice) * 100), // Convert to cents
+      currency: "usd",
+      metadata: { order_id: order.id },
+    });
+    client_secret = paymentIntent.client_secret;
+  }
+
   return (
     <OrderDetailsTable
       order={{
         ...order,
         shippingAddress: order.shippingAddress as ShippingAddress,
       }}
+      stripeClientSecret={client_secret}
       paypalClientId={process.env.PAYPAL_CLIENT_ID || "sb"}
       isAdmin={session?.user.role === "admin" || false}
     />
